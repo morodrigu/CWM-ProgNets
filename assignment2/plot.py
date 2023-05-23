f73fd88f6c08fa3ce1f5065a21cf638d8e5849ff
morodrigu <134230719+morodrigu@users.noreply.github.com>
2023-05-23 12:14:06 +0100

    Add files via upload

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -3,17 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # parameters to modify
-filename="test.data"
+filename="processed_iperf3.log"
 label='label'
-xlabel = 'xlabel'
-ylabel = 'ylabel'
-title='Simple plot'
-fig_name='test.png'
+xlabel = 'time'
+ylabel = 'Bandwidth [Mbit/s]'
+title='Bandwidth vs time, iperf3'
+fig_name='iperf3.png'
+bins=100 #adjust the number of bins to your plot
 
 
-t = np.loadtxt(filename, delimiter=" ", dtype="float")
-
-plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
+t = np.loadtxt(filename, dtype="float")
+plt.plot(np.linspace(0,10, len(t)), t, label=label)  # Plot some data on the (implicit) axes.
+#Comment the line above and uncomment the line below to plot a CDF
+#plt.hist(t, bins, density=True, histtype='step', cumulative=True, label=label)
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
 plt.title(title)
